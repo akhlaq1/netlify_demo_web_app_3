6f4d43eef6257a1d88b5c6f38f0223d2c6f4e2d8
akhlaq1 <akhlaq.computer@gmail.com>
2023-01-20 22:17:42 +0500

    href issue resolved

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -5,31 +5,31 @@ function Navigation() {
     <div>
       <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
   <div className="container-fluid">
-    <a className="navbar-brand" href="#">MyApp</a>
+    <a className="navbar-brand" href="https://react-suffah.netlify.app">MyApp</a>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
     <div className="collapse navbar-collapse" id="navbarSupportedContent">
       <ul className="navbar-nav me-auto mb-2 mb-lg-0">
         <li className="nav-item">
-          <a className="nav-link active" aria-current="page" href="#">Home</a>
+          <a className="nav-link active" aria-current="page" href="https://react-suffah.netlify.app">Home</a>
         </li>
         <li className="nav-item">
-          <a className="nav-link" href="#">Link</a>
+          <a className="nav-link" href="https://react-suffah.netlify.app">Link</a>
         </li>
         <li className="nav-item dropdown">
-          <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+          <a className="nav-link dropdown-toggle" href="https://react-suffah.netlify.app" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
             Dropdown
           </a>
           <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
-            <li><a className="dropdown-item" href="#">Action</a></li>
-            <li><a className="dropdown-item" href="#">Another action</a></li>
+            <li><a className="dropdown-item" href="https://react-suffah.netlify.app">Action</a></li>
+            <li><a className="dropdown-item" href="https://react-suffah.netlify.app">Another action</a></li>
             <li><hr className="dropdown-divider" /></li>
-            <li><a className="dropdown-item" href="#">Something else here</a></li>
+            <li><a className="dropdown-item" href="https://react-suffah.netlify.app">Something else here</a></li>
           </ul>
         </li>
         <li className="nav-item">
-          <a className="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+          <a className="nav-link disabled" href="https://react-suffah.netlify.app" tabindex="-1" aria-disabled="true">Disabled</a>
         </li>
       </ul>
       <form className="d-flex">
