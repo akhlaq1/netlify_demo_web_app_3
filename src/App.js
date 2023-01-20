0618cf66f77aab56a12c8e12d3dbadd2210540e3
akhlaq1 <akhlaq.computer@gmail.com>
2023-01-20 22:14:00 +0500

    login form complete

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 
 import './App.css';
+import Login from './components/Login';
+import Navigation from './components/Navigation';
+
 
 function App() {
   return (
     <div className="App">
-          <h1>Hello this is my website</h1>
+          <Navigation />
+          
+          <div className='container mt-5'>
+          <div className='display-1 mt-5 mb-5 text-center'>Login Form</div>
+          <Login />
+          </div>
+          
     </div>
   );
 }
